ba81a4aad474c6e23a2b42fea2e0e35b820d7aeb
Adedoyin <shukurah4254@gmail.com>
2022-04-12 12:45:44 -0700

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-#ifndef _MAIN_H_
 #define _MAIN_H_
 
 int _putchar(char c);
@@ -14,5 +13,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-end /* _MAIN_H_ */
-
