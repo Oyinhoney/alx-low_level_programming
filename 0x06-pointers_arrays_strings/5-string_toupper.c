436134719ac31e43b5240e1464c7e0b1b59a9b0e
Adedoyin <shukurah4254@gmail.com>
2022-04-24 03:48:29 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 /**
  * string_toupper - chamges all lowercase of a string to uppercase
- * @str: the pointer to the string
+ * @str: the poiter to the string
  * Return: return a pointer to the string
  */
 char *string_toupper(char *str)
