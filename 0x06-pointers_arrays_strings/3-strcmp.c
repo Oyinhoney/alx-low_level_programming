ee6b9640b657f03d75aa23b784e236a073adc1f9
Adedoyin <shukurah4254@gmail.com>
2022-04-24 03:26:18 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,5 @@
 #include "main.h"
-
-
+#include <string.h>
 /**
  * _strcmp - a function that compares two strings.
  * @s1: an input string
@@ -9,11 +8,5 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-			return (*s1 - s2);
-		s1++, s2++;
-	}
-	return (*s1 - *s2);
+	return (strcmp(s1, s2));
 }
