ebe437b7ba4a7af9f8a563e345e81ba91444fb93
Adedoyin <shukurah4254@gmail.com>
2022-05-30 08:32:15 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,22 +7,24 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
+	int c, j;
+	int flag = 0;
 
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	while (max)
+
+	for (c = 63; c >= 0; c--)
 	{
-		if (flag == 1 && (n & max) == 0)
-			_putchar('0');
-		else if ((n & max) != 0)
+		j = n >> c;
+		if (j & 1)
 		{
-			_putchar('1');
 			flag = 1;
+			_putchar('1');
 		}
-		max >>= 1;
+		else if (flag == 1)
+			_putchar('0');
 	}
 }
